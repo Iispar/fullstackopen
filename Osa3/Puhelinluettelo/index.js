315bfa94b8728b26c02924461d576f71566d6bd4
Iiro Partanen <iispar@utu.fi>
2022-06-01 12:35:10 +0300

    tehtävät 3.8 asti

--- Osa3/Puhelinluettelo/index.js
+++ Osa3/Puhelinluettelo/index.js
@@ -5,6 +5,10 @@ var morgan = require('morgan')
 
 app.use(express.json())
 
+morgan.token("body", req => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let persons = [
           {
             "name": "Arto Hellas",
