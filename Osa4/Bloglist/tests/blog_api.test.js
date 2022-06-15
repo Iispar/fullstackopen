3c5844109a6ea259a9d4d10dae0bb97e663ad897
Iiro Partanen <iispar@utu.fi>
2022-06-15 21:46:49 +0300

    4.10 tehty

--- Osa4/Bloglist/tests/blog_api.test.js
+++ Osa4/Bloglist/tests/blog_api.test.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
 
 test('notes are returned as json', async () => {
   await api.get('/api/blogs')
@@ -15,6 +40,29 @@ test('notes have an id field', async () => {
   const response = await api.get('/api/blogs')
   expect(response.body[0].id).toBeDefined()
 })  
+
+test('posting notes work', async () => {
+
+  const uusiBlog = {
+    title: 'Testi2',
+    author: 'testi2 author',
+    url: 'http://www.testi2.com',
+    likes: 100
+  }
+
+  await api.post('/api/blogs')
+    .send(uusiBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const res = await api.get('/api/blogs')
+  const contents = res.body.map(r => r.title)
+  expect(res.body).toHaveLength(initialBlogs.length + 1)
+  expect(contents).toContain('Testi2')
+
+
+})
+
 
 afterAll(() => {
   mongoose.connection.close()
