2d2f7901c2acb9fa859834bd3b5108f475c3a892
Iiro Partanen <iispar@utu.fi>
2022-08-17 18:27:54 +0300

    5.16 asti

--- Osa4/Bloglist/controllers/blogs.js
+++ Osa4/Bloglist/controllers/blogs.js
@@ -7,9 +7,11 @@ const jwt = require('jsonwebtoken')
 const getTokenFrom = request => {
   const authorization = request.get('authorization')
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
   }
-  return null
+  return request.token
 }
 
 blogsRouter.get('/', async (request, response) => {
@@ -22,14 +24,19 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const token = getTokenFrom(request)
+
   const decodedToken = jwt.verify(token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ 
       error: 'token missing or invalid'
     })
   }
-  const user = await User.findById(body.userId)
+
+  const user = await User.findById(decodedToken.id)
+
+  console.log(user)
 
   const blog = new Blog({
     title: body.title,
@@ -43,7 +50,7 @@ blogsRouter.post('/', async (request, response) => {
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
-  response.json(savedBlog)
+  response.json(savedBlog.toJSON())
 })
 
 
@@ -52,11 +59,12 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put(':id', async (request, response) => {
+  console.log('meni läpi')
   const { body } = request
   const { id } = request.params
-
   const blog = {
+    title: body.title,
     likes: body.likes,
     author: body.author,
     url: body.url
--- Osa4/Bloglist/controllers/login.js
+++ Osa4/Bloglist/controllers/login.js
@@ -5,18 +5,15 @@ const User = require('../models/user')
 
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body
-
-  const user = await User.findOne({ username })
+  const user = await User.findOne({username: username })
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
-
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: 'invalid username or password'
     })
   }
-
   const userForToken = {
     username: user.username,
     id: user._id,
@@ -24,10 +21,9 @@ loginRouter.post('/', async (request, response) => {
 
   const token = jwt.sign(userForToken, process.env.SECRET)
 
-  
   response
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, username: user.username, name: user.name, userId: user._id })
 })
 
 module.exports = loginRouter
